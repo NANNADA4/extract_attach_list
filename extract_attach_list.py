1e668456f288fb8f6574e9c1550456ef0d820503
NANNADA4 <coco96062233@icloud.com>
2024-09-23 15:13:47 +0900

    -

---
+++ extract_attach_list.py
@@ -0,0 +1,33 @@
+"""
+main 함수
+"""
+
+import os
+
+
+from module.process_folder import process_folder
+
+
+def main():
+    """main 함수. PDF의 4단계에서 별도제출자료 리스트를 추출합니다"""
+    print("\n>>>>>>별도제출자료 리스트 추출 (구축세부내역)<<<<<<\n")
+    print("-"*24)
+    input_path = input("폴더 경로를 입력하세요 (종료는 0을 입력) : ")
+
+    if input_path == '0':
+        return 0
+
+    if not os.path.isdir(input_path):
+        print("입력 폴더의 경로를 다시 한번 확인하세요")
+        return main()
+
+    input_path = os.path.join('\\\\?\\', input_path)
+    process_folder(input_path)
+
+    print("\n~~~폴더 내 PDF에서 별도제출자료 리스트 추출이 완료되었습니다~~~")
+
+    return main()
+
+
+if __name__ == "__main__":
+    main()
---
+++ module/process_folder.py
@@ -0,0 +1,63 @@
+"""
+폴더를 순회하면서 PDF 파일을 찾고 북마크를 가져와 별도제출자료 추출을 시도합니다
+"""
+
+import os
+import re
+from natsort import natsorted
+
+
+from module.create_excel import _load_excel, create_excel
+from module.create_log import logging
+from module.extract_bookmark import extract_bookmark
+
+
+def process_folder(input_path):
+    """폴더를 순회하면서 PDF 파일을 찾아 북마크 추출을 시도합니다"""
+    for root, _, files in os.walk(input_path):
+        for file in natsorted(files):
+            excel_list = []
+
+            if not file.lower().endswith('.pdf'):
+                continue
+            pdf_path = os.path.join(root, file)
+
+            first_underscore_index = file.find('_')
+            second_underscore_index = file.find(
+                '_', first_underscore_index + 1)
+            if first_underscore_index != -1 and second_underscore_index != -1:
+                cmt = file[first_underscore_index +
+                           1:second_underscore_index]
+            else:
+                cmt = ""
+
+            org_matches = re.findall(r'\(([^)]+)\)', file)
+            if org_matches:
+                org = org_matches[-1]
+                if org == '2':
+                    org = org_matches[-2]
+                if str(org).endswith('(주'):
+                    org = str(org).replace('(주', '(주)')
+            else:
+                org = ""
+
+            for item in extract_bookmark(pdf_path):
+                try:
+                    if len(item) <= 1 or item['level'] != 4:
+                        continue
+
+                    excel_list.append({
+                        "cmt": cmt,
+                        "org": org,
+                        "name": item['parent']['parent']['title'],
+                        "question": item['parent']['title'],
+                        "realfile_name": item['title'],
+                        "real_path": os.path.join(cmt, org, item['parent']['parent']['title'],
+                                                  item['title'])
+                    })
+                except Exception as e:  # pylint: disable=W0703
+                    e = "PDF 북마크 추출 오류"
+                    logging(e, '', input_path)
+
+            excel_path = os.path.join(input_path, 'SubmitList.xlsx')
+            create_excel(_load_excel(excel_path), excel_list, excel_path)
